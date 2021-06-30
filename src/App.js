2b11a77eae0661052723b61ea09e22f22b863dae
senbiki akifumi <akifumi.senbiki.1209@gmail.com>
2021-07-01 00:21:28 +0900

    ルーティング検証前コミット

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import Shop  from "./components/Shop"
 import Modal from "./components/Modal";
 import axios from "axios";
 function App() {
+
+
+  const [count, setCount] = useState(0)
+  const [quotient, setQuotient] = useState (0);
+
+  const countUp = () => {
+    // setCount((pre) => {
+    //   let temp = pre + 1;
+    //   return temp;
+    // });
+    setCount (count + 1);
+    if (count % 3 === 0) {
+      setQuotient(count / 3);
+    } else {
+      setQuotient(0);
+    }
+  }
+
+  const countDown = () => {
+    setCount(count - 1);
+    if (count % 3 === 0) {
+      setQuotient(count / 3);
+    } else {
+      setQuotient(0);
+    }
+  };
+
+  const resetButton = () => {
+    setCount(0);
+  }
+  const doubleUp = () => {
+    setCount(count * 2);
+    if (count % 3 === 0) {
+      setQuotient(count / 3);
+    } else {
+      setQuotient(0);
+    }
+  }
 
   const [isDisplayed, setIsDisplayed] = React.useState(false);
   const [ shopList , setShopList] = React.useState([]);
@@ -36,6 +74,15 @@ function App() {
 
   return (
     <React.Fragment>
+      <div>
+        <p> count => {count}</p>
+        <p onClick={countUp}>カウントアップ</p>
+        <p onClick={countDown}>カウントダウン</p>
+        <p onClick={resetButton}>リセット</p>
+        <p onClick={doubleUp}>2倍</p>
+        <p>商 => {quotient}</p>
+      </div>
+
       <Shop delegateShopData={delegateShopData} />
       <hr/>
       <p>以下は､Appコンポーネント内処理です｡</p>

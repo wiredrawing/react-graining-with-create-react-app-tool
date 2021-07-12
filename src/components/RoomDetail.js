a161660c572ffa2302a5285204645618cc013fda
senbiki akifumi <akifumi.senbiki.1209@gmail.com>
2021-07-12 10:27:21 +0900

    websocketをつかったリアルタイム処理を追加

--- src/components/RoomDetail.js
+++ src/components/RoomDetail.js
@@ -11,8 +11,16 @@ import { useHistory, useLocation, useParams } from "react-router-dom";
 
 export const RoomDetail = () => {
 
+
+  let wsUrl = "ws://localhost";
+  let wsPort = 5001;
+  let wsRequestUrl = wsUrl + ":" + wsPort;
+
   const params = useParams();
   const history = useHistory();
+
+  // トークルームやり取り時のwebソケットの取り回し
+  const [websocket, setWebsocket] = useState(null);
 
   const currentRoomId = params.id;
   let location  = useLocation();
@@ -30,22 +38,22 @@ export const RoomDetail = () => {
     {
       id:1,
       user: "やまだ",
-      message: "ああああああああああああああああ",
+      message: "ここにメッセージが表示されます",
     },
     {
       id:2,
       user: "きむら",
-      message: "いいいいいいいいいいいいいいいい",
+      message: "ここにメッセージが表示されます",
     },
     {
       id:4,
       user: "たなか",
-      message: "うううううううううううううううう",
+      message: "ここにメッセージが表示されます",
     },
     {
       id:10,
       user: "たなか",
-      message: "ええええええええええええええええ",
+      message: "ここにメッセージが表示されます",
     },
   ]);
 
@@ -62,8 +70,9 @@ export const RoomDetail = () => {
     }
     axios.post("http://localhost:8000/api/room/" + currentRoomId + "/message", post).then((data) => {
       axios.get("http://localhost:8000/api/room/" + currentRoomId).then((data) => {
-        setTalkList(data.data);
+        // setTalkList(data.data);
         setMessage("");
+        websocket.send(currentRoomId)
         document.getElementById("chat-block").scrollIntoView({ block: "end" });
       });
     });
@@ -75,12 +84,44 @@ export const RoomDetail = () => {
   //   });
   // }, 10000);
 
+
+  let ws = null;
   useEffect(() => {
     axios.get("http://localhost:8000/api/room/" + currentRoomId).then((data) => {
-      setTalkList(data.data);
+      // setTalkList(data.data);
       document.getElementById("chat-block").scrollIntoView({ block: "end" });
     });
-  }, []);
+
+    // websocketを指定のURLへリクエスト
+    ws = new WebSocket(wsRequestUrl + "/api/room/" + currentRoomId);
+    setWebsocket(ws);
+  }, [
+    // 監視対象が空の配列のため､初回のみ実行される
+  ]);
+
+
+  useEffect(() => {
+    if (websocket !== null) {
+      websocket.addEventListener("open", (e) => {
+        console.log("executed websocket open event. => ", e);
+        // web socketサーバーへのデータ送信
+        let seed = {
+          auth_info: "クライアント識別用情報",
+          room_id: currentRoomId,
+        };
+        websocket.send(currentRoomId)
+      });
+
+      websocket.addEventListener("message", (e) => {
+        console.log("==> サーバーから取得した内容", e);
+        console.log(JSON.parse(e.data));
+        setTalkList(JSON.parse(e.data));
+        document.getElementById("chat-block").scrollIntoView({ block: "end" });
+      });
+    }
+  }, [websocket]);
+
+
 
   const backRoomListPage = (e) => {
     history.push("/room");
@@ -89,19 +130,8 @@ export const RoomDetail = () => {
   return (
     <React.StrictMode>
       <div className="container">
-
         <div className="row">
-        <div className="col-12">
-            <input type="text" onChange={registerUserName} value={userName}></input>
-          </div>
-          <div className="col-12">
-            <input type="text" onChange={newMessage} value={message}></input>
-          </div>
-          <div className="col-12">
-            <button type="button" onClick={talkNewMessage}>発言</button>
-          </div>
-        </div>
-        <div className="chat-block" id="chat-block">
+          <div className="chat-block" id="chat-block">
           {talkList.map((talk) => {
             return (
               <div className="row" key={talk.id}>
@@ -114,10 +144,22 @@ export const RoomDetail = () => {
               </div>
             )
           })}
+          </div>
           <div className="row">
             <button type="button" onClick={backRoomListPage} className="form-control btn btn-outline-primary">Room一覧へ戻る</button>
           </div>
         </div>
+        <div className="row">
+            <div className="col-12">
+              <input type="text" onChange={registerUserName} value={userName}></input>
+            </div>
+            <div className="col-12">
+              <input type="text" onChange={newMessage} value={message}></input>
+            </div>
+            <div className="col-12">
+              <button type="button" onClick={talkNewMessage}>発言</button>
+            </div>
+          </div>
       </div>
     </React.StrictMode>
   )
